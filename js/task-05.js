4dbb6831d73cc4d35b2fb812d18c7817619c5587
Vaddex <89461632+Vaddex@users.noreply.github.com>
2023-10-13 13:23:53 +0300

    added 5 task content

---
+++ js/task-05.js
@@ -0,0 +1,8 @@
+"use strict";
+const inputForm = document.querySelector("#name-input");
+const outputString = document.querySelector("#name-output");
+
+inputForm.addEventListener("input", handlerInput);
+function handlerInput(event) {
+  outputString.textContent = event.currentTarget.value || "Anonimus";
+}
