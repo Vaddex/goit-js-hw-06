3004246040c811552023d7e65969fdb02b67275f
Vaddex <89461632+Vaddex@users.noreply.github.com>
2023-10-14 22:40:54 +0300

    added 9 task content

--- js/task-09.js
+++ js/task-09.js
@@ -1,5 +1,17 @@
+"use strict";
 function getRandomHexColor() {
   return `#${Math.floor(Math.random() * 16777215)
     .toString(16)
     .padStart(6, 0)}`;
+}
+
+const bodyContainer = document.querySelector("body");
+const btnColorChanger = document.querySelector(".change-color");
+const spanValue = document.querySelector(".color");
+
+btnColorChanger.addEventListener("click", changeBodyColor);
+function changeBodyColor() {
+  const changeColor = getRandomHexColor();
+  bodyContainer.style.backgroundColor = `${changeColor}`;
+  spanValue.textContent = `${bodyContainer.style.backgroundColor}`;
 }
